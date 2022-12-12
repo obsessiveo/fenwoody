03113c320b9c6198ec1439589229c15463760ce4
Obsessiveo <obsessiveo@protonmail.com>
2022-12-12 18:49:39 +0100

    validate fen function

--- fenwoody.test.ts
+++ fenwoody.test.ts
@@ -1,4 +1,5 @@
 import { convertFenToSvg } from './src/convert-fen';
+import { validateFen } from './src/fen';
 import { FenWoodyOptions, initialFEN } from './src/types';
 
 describe('Fenwoody', () => {
@@ -15,6 +16,27 @@ describe('Fenwoody', () => {
   test('should return a valid svg', () => {
     // expect(svg).toMatch(/^<svg/);
     expect(svg).toMatch(/<svg(.*)>[^]*<\/svg>/m);
+  });
+});
+
+const FenStrings = [
+  {
+    fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
+    valid: true,
+  },
+  {
+    fen: 'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2',
+    valid: true,
+  },
+  {
+    fen: 'rnbqkbnr/pp1ppppp/81/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2',
+    valid: false,
+  },
+];
+
+describe('Validate a fen string', () => {
+  test.each(FenStrings)('should return $valid for $fen', ({ fen, valid }) => {
+    expect(validateFen(fen)).toBe(valid);
   });
 });
 
--- src/fen.ts
+++ src/fen.ts
@@ -45,3 +45,52 @@ export function fenToPieces(fen: woodyTypes.FEN): woodyTypes.Pieces {
   }
   return pieces;
 }
+
+/**
+ *
+ * @param fen The fen string
+ * @returns true if the fen is valid, false otherwise
+ */
+export function validateFen(fen: woodyTypes.FEN): boolean {
+  // from https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
+
+  // 1. A FEN record contains six fields, each separated by a space.
+  const chunks = fen.split(' ');
+  if (chunks.length !== 6) return false;
+
+  const [piecePlacement, activeColor, castling, enPassant, halfMoveClock, fullMoveNumber] = chunks;
+
+  // 2. The first field contains the placement of the pieces on the board.
+  // The board is divided into eight rows, and each row contains eight fields.
+  const ranks = piecePlacement.split('/');
+  if (ranks.length !== 8) return false;
+  for (const rank of ranks) {
+    let sum = 0;
+    for (const c of rank) {
+      if ('12345678'.includes(c)) {
+        sum += parseInt(c, 10);
+      } else if ('prnbqkPRNBQK'.includes(c)) {
+        sum++;
+      } else {
+        return false;
+      }
+    }
+    if (sum !== 8) return false;
+  }
+
+  // 3. The second field contains the active color.
+  if (!/^[wb]$/.test(activeColor)) return false;
+
+  // 4. The third field contains castling availability.
+  if (!/^[KQkq-]{1,4}$/.test(castling)) return false;
+
+  // 5. The fourth field contains the en passant target square.
+  if (!/^[a-h1-8-]{1,2}$/.test(enPassant)) return false;
+
+  // 6. The fifth field contains the halfmove clock.
+  if (!/^[0-9]{1,2}$/.test(halfMoveClock)) return false;
+
+  // 7. The sixth field contains the fullmove number.
+  if (!/^[0-9]{1,4}$/.test(fullMoveNumber)) return false;
+  return true;
+}
